97c42bf9f450b47d67a8b71a017f16f26ac34314
Ashish212020 <tiwariashish01332@gmail.com>
2025-11-25 02:57:38 +0530

    some fix

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -47,8 +47,7 @@ export const useAuthStore = create((set, get) => ({
       const res = await axiosInstance.post("/auth/login", data);
       set({ authUser: res.data });
       toast.success("Logged in successfully");
-get().connectSocket()
-      
+      get().connectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
     } finally {
