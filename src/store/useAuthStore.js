6c566705c8395162d62dc22cc5d06067f09e9ae2
Ashish212020 <tiwariashish01332@gmail.com>
2025-11-25 13:17:55 +0530

    taostify removed

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import {io} from "socket.io-client"
-import { toast } from "react-toastify";
+
 
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8080" : "https://talktive-backend.onrender.com";
 
